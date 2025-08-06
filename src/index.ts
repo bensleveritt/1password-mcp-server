9dfc396a4d6bd7c41460d17ff63c9175bfef688c
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2025-08-06 13:05:31 +0100

    Adds get secure note tool

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,16 @@
 
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import { vault, item, validateCli, version } from "@1password/op-js";
+import {
+  vault,
+  item,
+  validateCli,
+  version,
+  ValueField,
+} from "@1password/op-js";
 
-const userVault = process.env.OP_VAULT;
+const OP_VAULT = process.env.OP_VAULT;
+const OP_NOTE_NAME = "Private Chat Note";
 
 const server = new McpServer({
   name: "basic-mcp-server",
@@ -32,9 +39,9 @@ server.registerTool(
 
 // List items in vault
 server.registerTool(
-  "list-items",
+  "list-vault-items",
   {
-    title: "List Items",
+    title: "List Vault Items",
     description: "List items in a 1Password vault",
   },
   async () => {
@@ -51,13 +58,50 @@ server.registerTool(
 
     validateCli();
 
-    const items = item.list({ vault: userVault });
+    const items = item.list({ vault: OP_VAULT });
 
     return {
       content: [
         {
           type: "text",
-          text: `There are ${items.length} items in the "${userVault}" vault`,
+          text: `There are ${items.length} items in the "${OP_VAULT}" vault`,
+        },
+      ],
+    };
+  }
+);
+
+// Get secure note
+server.registerTool(
+  "get-secure-note",
+  {
+    title: "Get Secure Note",
+    description: "Get a secure note from a 1Password vault",
+  },
+  async () => {
+    if (!OP_VAULT || !OP_NOTE_NAME) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: "Vault hasn't been specified. To use this tool, set the OP_VAULT environment variable.",
+          },
+        ],
+      };
+    }
+
+    validateCli();
+
+    const note = item.get(OP_NOTE_NAME, {
+      vault: OP_VAULT,
+      fields: { label: ["notesPlain"] },
+    }) as ValueField;
+
+    return {
+      content: [
+        {
+          type: "text",
+          text: note.value,
         },
       ],
     };
